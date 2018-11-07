0203f39cf391ba1bf0ecb4b46b6164684fc4ecb4
streanga-sarmis <streangasarmis@gmail.com>
2018-11-07 03:18:52 +0200

    Changed values for testing

--- avr_test.c
+++ avr_test.c
@@ -4,11 +4,12 @@
 #include "easy_serial.h"
 
 int main (){
+    serial_usart_init();
     for(;;){
-        serial_usart_write(0x69);
-        serial_usart_write(0x00);
-        serial_usart_write(0x00);
-        serial_usart_write(0x69);
+        serial_usart_write(0x70);
+        serial_usart_write(0x71);
+        serial_usart_write(0x71);
+        serial_usart_write(0x70);
         _delay_ms(500);
     }
 }
